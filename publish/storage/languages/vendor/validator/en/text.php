d11e46f9b50d9a75adc2884b0754d6e7e0f8372a
Iqbal Fauzi <iqbalfawz@gmail.com>
2021-11-17 12:47:06 +0700

    Remove empty array from translation

--- publish/storage/languages/vendor/validator/en/text.php
+++ publish/storage/languages/vendor/validator/en/text.php
@@ -5,6 +5,6 @@ return [
     'not a text' => 'The value is not a text',
     'not an alnum' => 'The value is not valid alnum',
     'not an alnumdash' => 'The value is not valid alnumdash',
-    'not an alpha' => 'The value is not valid alpha',,
+    'not an alpha' => 'The value is not valid alpha',
     'not match with the regex' => 'The value is not match as requested format'
 ];
